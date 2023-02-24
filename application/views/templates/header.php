43b82e22374fca3dee08cc0502a59946e188c00b
kampusinformatika <kanglerian.business@gmail.com>
2023-02-24 10:46:43 +0700

    tambah meta

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Politeknik LP3I Kampus Tasikmalaya adalah perguruan tinggi swasta di Kota Tasikmalaya berbasis pendidikan vokasi yaitu 70% praktek dan 30% teori. Mahasiswa/i Politeknik LP3I Kampus Tasikmalaya di semester 6 akan ditempatkan bekerja oleh bagian Career Center di perusahaan relasi. Mandiri, tepat dan cepat kerja!">
+  <meta name="keywords" content="Kuliah, Penempatan Kerja, Kampus Vokasi, Beasiswa Pendidikan, Perguruan Tinggi, Politeknik, LP3I, Kota Tasikmalaya, Vokasi, Kuliah Praktek">
+  <meta name="author" content="Politeknik LP3I Kampus Tasikmalaya">
 	<title>Politeknik LP3I Kampus Tasikmalaya</title>
 	<link rel="icon" type="image/x-icon" href="<?= base_url() ?>public/assets/favicon.png">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
