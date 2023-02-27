ba48fc7132e35b74fc40637296489ef1c465deca
kampusinformatika <kanglerian.business@gmail.com>
2023-02-27 10:49:48 +0700

    change account

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-/* $config['base_url'] = 'https://politekniklp3i-tasikmalaya.ac.id/'; */
-$config['base_url'] = 'http://localhost/lp3itasik';
+$config['base_url'] = 'https://politekniklp3i-tasikmalaya.ac.id/';
+/* $config['base_url'] = 'http://localhost/lp3itasik'; */
 
 /*
 |--------------------------------------------------------------------------
