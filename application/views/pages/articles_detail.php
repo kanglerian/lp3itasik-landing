93144ab04f8636e7ff8a9d596ea78a53717c6aee
kampusinformatika <kanglerian.business@gmail.com>
2023-03-10 11:59:38 +0700

    order

--- application/views/pages/articles_detail.php
+++ application/views/pages/articles_detail.php
@@ -33,7 +33,7 @@
 <section class="my-14">
 	<div class="container mx-auto px-4">
 		<div class="flex flex-col justify-end md:flex-row gap-5">
-			<div class="w-full md:w-1/3">
+			<div class="w-full md:w-1/3 order-2 md:order-none">
 				<h1 class="text-xl font-bold text-gray-800">
 					<?= $this->session->userdata('language') == 'en' ? 'More News' : 'Berita Lainnya' ?></h1>
 				<hr class="my-2">
@@ -51,7 +51,7 @@
 					</ul>
 				</div>
 			</div>
-			<div class="w-full md:w-2/3">
+			<div class="w-full md:w-2/3 order-1 md:order-none">
 				<img src="<?= base_url() ?>uploads/<?= $article->image ?>" alt="<?= $article->title ?>" class="rounded-xl shadow border-8 border-white">
 				<div class="space-y-5 mt-4 bg-gray-100 p-5 rounded-xl">
 					<h1 class="text-4xl font-bold text-gray-800"><?= $article->title ?></h1>
