4fbdc11512d44f01b68ff3078abfd1f902f27fb3
kampusinformatika <kanglerian.business@gmail.com>
2023-03-10 13:09:15 +0700

    ubah posisi

--- application/views/pages/blogs.php
+++ application/views/pages/blogs.php
@@ -1,20 +1,23 @@
 <section class="my-14">
 	<div class="container mx-auto px-4">
+
 		<header class="mb-7 text-center">
-		<a href="<?= base_url() ?>blogs/articles"><h1 class="text-3xl font-bold text-gray-700 hover:text-gray-800 underline underline-offset-8">Artikel Terbaru <i class="ml-2 fa-solid fa-circle-arrow-right"></i></h1></a>
+			<a href="<?= base_url() ?>blogs/medias">
+				<h1 class="text-3xl font-bold text-gray-700 hover:text-gray-800 underline underline-offset-8">Media Terbaru <i class="ml-2 fa-solid fa-circle-arrow-right"></i></h1>
+			</a>
 		</header>
-		<?php if (!empty($articles)) { ?>
+		<?php if (!empty($medias)) { ?>
 			<div class="w-full flex justify-center flex-wrap gap-5">
-				<?php foreach ($articles as $article) { ?>
+				<?php foreach ($medias as $media) { ?>
 					<div class="item w-96 h-auto border-8 border-white shadow rounded-lg ease-in-out delay-50 md:hover:-translate-y-1 md:hover:scale-105 duration-300">
-						<img src="<?= base_url() ?>uploads/<?= $article->image ?>" alt="<?= $article->title ?>" class="rounded-lg">
+						<img src="<?= base_url() ?>uploads/<?= $media->image ?>" alt="<?= $media->title ?>" class="rounded-lg">
 						<div class="p-4 space-y-4">
-							<h5 class="font-bold text-base text-left text-gray-700"><?= substr($article->title, 0, 70) ?><?= strlen($article->title) >=70 ? '...' : '' ?></h5>
-							<div class="flex justify-between items-center">
-								<a href="<?= base_url() ?>blogs/article/<?= $article->uuid ?>" role="button" class="bg-cyan-600 text-white text-xs py-1 px-3 rounded-md">
+							<h5 class="font-bold text-base mb-1 text-left text-gray-700"><?= substr($media->title, 0, 70) ?><?= strlen($media->title) >= 70 ? '...' : '' ?></h5>
+							<div class="flex justify-between items-center mt-3">
+								<a href="<?= base_url() ?>blogs/media/<?= $media->uuid ?>" role="button" class="bg-cyan-600 text-white text-xs py-1 px-3 rounded-md">
 									<?= $this->session->userdata('language') == 'en' ? 'View more' : 'Lihat selengkapnya' ?>
 								</a>
-								<span class="block bg-gray-200 text-gray-500 text-xs py-1 px-3 rounded-md"><?= date("d F Y", strtotime($article->date)) ?></span>
+								<span class="block bg-gray-200 text-gray-500 text-xs py-1 px-3 rounded-md"><?= date("d F Y", strtotime($media->date)) ?></span>
 							</div>
 						</div>
 					</div>
@@ -27,20 +30,22 @@
 		<?php } ?>
 		<hr class="my-12">
 		<header class="mb-7 text-center">
-			<a href="<?= base_url() ?>blogs/medias"><h1 class="text-3xl font-bold text-gray-700 hover:text-gray-800 underline underline-offset-8">Media Terbaru <i class="ml-2 fa-solid fa-circle-arrow-right"></i></h1></a>
+			<a href="<?= base_url() ?>blogs/articles">
+				<h1 class="text-3xl font-bold text-gray-700 hover:text-gray-800 underline underline-offset-8">Artikel Terbaru <i class="ml-2 fa-solid fa-circle-arrow-right"></i></h1>
+			</a>
 		</header>
-		<?php if (!empty($medias)) { ?>
+		<?php if (!empty($articles)) { ?>
 			<div class="w-full flex justify-center flex-wrap gap-5">
-				<?php foreach ($medias as $media) { ?>
+				<?php foreach ($articles as $article) { ?>
 					<div class="item w-96 h-auto border-8 border-white shadow rounded-lg ease-in-out delay-50 md:hover:-translate-y-1 md:hover:scale-105 duration-300">
-						<img src="<?= base_url() ?>uploads/<?= $media->image ?>" alt="<?= $media->title ?>" class="rounded-lg">
+						<img src="<?= base_url() ?>uploads/<?= $article->image ?>" alt="<?= $article->title ?>" class="rounded-lg">
 						<div class="p-4 space-y-4">
-							<h5 class="font-bold text-base mb-1 text-left text-gray-700"><?= substr($media->title, 0, 70) ?><?= strlen($media->title) >=70 ? '...' : '' ?></h5>
-							<div class="flex justify-between items-center mt-3">
-								<a href="<?= base_url() ?>blogs/media/<?= $media->uuid ?>" role="button" class="bg-cyan-600 text-white text-xs py-1 px-3 rounded-md">
+							<h5 class="font-bold text-base text-left text-gray-700"><?= substr($article->title, 0, 70) ?><?= strlen($article->title) >= 70 ? '...' : '' ?></h5>
+							<div class="flex justify-between items-center">
+								<a href="<?= base_url() ?>blogs/article/<?= $article->uuid ?>" role="button" class="bg-cyan-600 text-white text-xs py-1 px-3 rounded-md">
 									<?= $this->session->userdata('language') == 'en' ? 'View more' : 'Lihat selengkapnya' ?>
 								</a>
-								<span class="block bg-gray-200 text-gray-500 text-xs py-1 px-3 rounded-md"><?= date("d F Y", strtotime($media->date)) ?></span>
+								<span class="block bg-gray-200 text-gray-500 text-xs py-1 px-3 rounded-md"><?= date("d F Y", strtotime($article->date)) ?></span>
 							</div>
 						</div>
 					</div>
